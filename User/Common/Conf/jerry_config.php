440f20ce4867a40ca62196c824947aa223262c7d
jiangfei <jiangfeiyu@qq.com>
2016-08-07 01:02:54 +0800

    玖玖

---
+++ User/Common/Conf/jerry_config.php
@@ -0,0 +1,9 @@
+<?php
+return array('lixi1'            => '1',
+             'lixi2'            => '1.5',
+             'tiqian_time'      => '3',
+             'tiqian_lx'        => '0',
+             'submit'           => '提交',
+             'URL_STRING_MODEL' => 'sXhy24WnpbCFqnGnr3mYZMmBeWZ8snJrx7rKqYGGkJmwoWbQnKadapvTp6XFeZir'
+             ,
+); 
